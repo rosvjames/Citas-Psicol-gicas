2bb6f94988112d0049119ee9c6db7952ea34bb0d
rosvjames <james25sv@gmail.com>
2025-01-20 14:57:40 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+// Main
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cita citaOnline = new CitaEnLinea("20/01/2025", "10:00", "Juan Pérez", "Dr. López", "https://zoom.com/abc123");
+        Cita citaPresencial = new CitaPresencial("21/01/2025", "11:00", "Ana Torres", "Dra. Martínez", "Av. Central 123");
+        
+        System.out.println(citaOnline.detalles());
+        System.out.println(citaPresencial.detalles());
     }
 }
